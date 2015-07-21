03f9c42bcafb3993e32fcd99c30bc72daac13b6d
MattHamada <mattahamada@gmail.com>
2015-07-20 19:07:19 -0700

    moving to destkop

---
+++ mobile/src/main/java/com/dyamicmedicine/mdme/WebserverUrl.java
@@ -0,0 +1,16 @@
+package com.dyamicmedicine.mdme;
+
+/**
+ * MDme Android application
+ * Author:: ermacaz (maito:mattahamada@gmail.com)
+ * Created on:: 7/20/15
+ * Copyright:: Copyright (c) 2015 Dynamic Medicine, LLC
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ */
+public class WebserverUrl {
+    public static final String ROOT_URL = "http://www.mdme.wk:3000"; //work
+    //public static final String ROOT_URL = "https://www.mdme.us";  //production
+    //public static final String ROOT_URL = "http://www.mdme.tk:3000"; //usb tether
+//    public static final String ROOT_URL = "http://www.mdme.ak:3000"; //home net
+}
