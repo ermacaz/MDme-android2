18771c4276119623ba199393cd3699c02e2fe5e7
MattHamada <mattahamada@gmail.com>
2015-08-03 10:01:53 -0700

    upcoming appointment singleton started

---
+++ mobile/src/main/java/com/dynamicmedicine/mdme/UpcomingAppointment.java
@@ -0,0 +1,17 @@
+package com.dynamicmedicine.mdme;
+
+/**
+ * MDme Android application
+ * Author:: ermacaz (maito:mattahamada@gmail.com)
+ * Created on:: 7/28/15
+ * Copyright:: Copyright (c) 2015 Dynamic Medicine, LLC
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential.
+ */
+public class UpcomingAppointment {
+
+    private static UpcomingAppointment mInstance = null;
+
+    private String mId;
+
+}
